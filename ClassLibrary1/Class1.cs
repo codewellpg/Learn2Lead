22fde156eaea5f6560dce73755cdba376f8b9362
pgutta <puneel.gutta@gmail.com>
2013-08-13 12:08:24 -0500

    HeadSpring LCE first version
    First Version. Trying git

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCEClassLibrary1
+{
+    public interface ILCE
+    {
+        string PrintNumbers(Range range,IDictionary<int,string> replaceMatrix);
+    }
+
+    public class Range
+    {
+        public Range()
+        {
+            LowerRange = 1;
+            UpperRange = 100;
+        }
+        public Range(int lowerRange, int upperRange)
+        {
+            LowerRange = lowerRange;
+            UpperRange = upperRange;
+        }
+        public int LowerRange { get; set; }
+        public int UpperRange { get; set; }
+    }
+
+    public class LCE: ILCE
+    {        
+        public string PrintNumbers(Range range = null, IDictionary<int,string> replaceWith = null)
+        {
+            StringBuilder s = new StringBuilder();
+            //If no range is specified then get the default range 
+            if (range == null)
+            {
+                range = new Range();
+            }
+            //if no reaplce parameters are passed in, replace with the following. 
+            if (replaceWith == null)
+            {
+                replaceWith.Add(3, "FIZZ");
+                replaceWith.Add(5, "BUZZ");
+            }
+            for (int i = (range.LowerRange < range.UpperRange ? range.LowerRange : range.UpperRange) ; 
+                        i <= (range.UpperRange > range.LowerRange ? range.UpperRange : range.LowerRange); i++)
+            {
+                StringBuilder temp = new StringBuilder();
+
+                if (replaceWith != null && replaceWith.Count > 0)
+                {
+                    foreach (var rNumber in replaceWith)
+                    {
+                        //You don't want to divide by 0 for obvious reasons.
+                        //Devided by 1 should be handled only for equal case than divided by case
+                        if (rNumber.Key != 1 && rNumber.Key != 0)
+                        {
+                            if (i % rNumber.Key == 0)
+                            {
+                                temp.Append(rNumber.Value);
+                            }
+                        }
+                        else
+                        {
+                            if (i == rNumber.Key)
+                            {
+                                temp.Append(rNumber.Value);
+                            }
+                        }
+                    }
+                    if (temp.Length == 0)
+                    {
+                        temp.Append(i);
+                    }
+                }
+                else
+                {
+                    temp.Append(i);
+                }
+                s.Append(temp.ToString());
+                s.Append("\n");
+            }
+            return s.ToString();
+        }
+    }
+
+    public class Excecise
+    {
+        private ILCE lce;
+        public Excecise(ILCE lce)
+        {
+            this.lce = lce;
+        }
+
+        public Range range = null;
+        public IDictionary<int, string> replaceMatrix = null;
+
+        public string PrintNumbersSeries()
+        {
+            return lce.PrintNumbers(range, replaceMatrix);
+        }
+    }
+}
---
+++ HeadSpringLce/LCE1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LCEClassLibrary1;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ILCE lce = new LCEClassLibrary1.Fakes.StubILCE()
+            {
+                PrintNumbersRangeIDictionaryOfInt32String = (range,replaceMatrix) => { return "ONE"; }
+            };
+
+            var underTest = new Excecise(lce);
+
+            string actualValue = underTest.PrintNumbersSeries();
+
+            Assert.AreEqual("ONE", actualValue);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            LCE lce = new LCE();
+
+            string expected = "1\n2\n3\n4\n";
+
+            string actual = lce.PrintNumbers(new Range(1, 4), new Dictionary<int, string> { });
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
